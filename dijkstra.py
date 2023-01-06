fbf94d7c9db6ac760660c0c43c5baf55f6af59c2
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-01-06 15:32:40 +0100

    implemented dijkstra algorithm

---
+++ dijkstra.py
@@ -0,0 +1,33 @@
+import heapq
+
+def dijkstra(graph, start, end):
+    # Mappa dei predecessori
+    predecessors = {}
+    # Mappa dei pesi
+    weights = {}
+    # Inizializzazione delle mappe
+    for node in graph:
+        weights[node] = float('inf')
+        predecessors[node] = None
+    weights[start] = 0
+    # Inserimento dei nodi nella coda di priorità
+    queue = []
+    for node in graph:
+        heapq.heappush(queue, (weights[node], node))
+    # Ciclo principale
+    while queue:
+        current_weight, current_node = heapq.heappop(queue)
+        if current_node == end:
+            # Costruzione del percorso ottimo
+            path = []
+            while current_node is not None:
+                path.append(current_node)
+                current_node = predecessors[current_node]
+            return path[::-1]
+        for neighbor, weight in graph[current_node].items():
+            new_weight = current_weight + weight
+            if new_weight < weights[neighbor]:
+                weights[neighbor] = new_weight
+                predecessors[neighbor] = current_node
+                heapq.heappush(queue, (new_weight, neighbor))
+    return []
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import json
 import matplotlib.pyplot as plt
 import networkx as nx
 
+from dijkstra import *
+
 # Load the graph data from the JSON file
 with open('graph1.json', encoding='utf-8-sig') as f:
     data = json.load(f)
 
-# Create a directed, weighted graph using the NetworkX library
+# Creazione del grafo con Network X
 G = nx.DiGraph()
 
 
@@ -14,13 +16,19 @@ for node in data['nodes']:
     G.add_node(node['id'])
 for arc in data['arcs']:
     G.add_edge(arc['end1'], arc['end2'], weight=arc['weight'])
+##############################################################
+
+# Creazione del grafo da analizzare con Dijkstra
+graph = {}
+for node in data['nodes']:
+    graph[node['id']] = {}
+for arc in data['arcs']:
+    graph[arc['end1']][arc['end2']] = arc['weight']
 
 
-shortest_path = nx.dijkstra_path(G, 0, 5)
-
-
+shortest_path = dijkstra(graph, 0, 5)
 print(shortest_path)
 
-
+# Plotting del grafo con networkx e matplot
 nx.draw(G, with_labels=True)
 plt.show()
