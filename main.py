f5253da8bca96a943df06a4af74dff924484b8d4
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-01-06 12:39:05 +0100

    version with networkx

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
-import os
-import json  # standard library no need to install
-
-
-import Graph  # weighted graph
-from dijkstra import *
-
-import igraph as ig
+import json
 import matplotlib.pyplot as plt
+import networkx as nx
+
+# Load the graph data from the JSON file
+with open('graph1.json', encoding='utf-8-sig') as f:
+    data = json.load(f)
+
+# Create a directed, weighted graph using the NetworkX library
+G = nx.DiGraph()
 
 
-if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    with open('graph1.json', encoding='utf-8-sig') as jfile:
-        jgraph = json.load(jfile)
-    print(jgraph['name'])
-    G = Graph.Graph(isDirected=jgraph['isDirected'])
-    for n in jgraph['nodes']:
-        G.addNode(n)
-    for m in jgraph['arcs']:
-        G.addEdge(m['end1'], m['end2'], (m['weight'] if "weight" in m.keys() else 1))
-
-    g = ig.Graph(directed=True)
-    g.add_vertices(6)
-    g.add_edges([(0, 1), (1, 0), (0, 2),(1, 2), (2, 1), (1, 3),(3, 2), (2, 3), (2, 4), (4, 2),(3, 5), (4, 3), (5, 4)])
-    # Generate the graph with its capacities
-    g.vs["name"] = ["s", "v1", "v2", "v3", "v4", "t"]
-    g.es["weight"] = [12, 4, 13, 10, 4, 8, 5, 4, 10, 4, 20, 7, 4]  # weight of each edge
-    g.es["capacity"] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # capacity of each edge
-    g.es["width"] = [3, 1, 1, 3, 1, 1, 1, 1, 3, 1, 3, 3, 1]  # drawing width of each edge
-    g.es["color"] = ['#F00', '#666', '#666', '#F00', '#666', '#666', '#666', '#666', '#F00',
-                     '#666', '#F00', '#F00', '#666']
-    grid = [[0, 2], [2, 4], [2, 0], [4, 4], [4, 0], [6, 2]]
-
-    G.printAdjlists()
-    print(dijkstra(G, 0))
-
-    fig, ax = plt.subplots()
-    ig.plot(
-        g,
-        target=ax,
-        layout=grid,
-        vertex_size=0.5,
-        vertex_color='lightblue',
-        vertex_label=g.vs['name'],
-        edge_width=g.es['width'],
-        edge_label=g.es["weight"],
-        edge_color=g.es["color"],
-        edge_align_label=True,
-        edge_background='white'
-    )
-    plt.show()
+for node in data['nodes']:
+    G.add_node(node['id'])
+for arc in data['arcs']:
+    G.add_edge(arc['end1'], arc['end2'], weight=arc['weight'])
 
 
+shortest_path = nx.dijkstra_path(G, 0, 5)
+
+
+print(shortest_path)
+
+
+nx.draw(G, with_labels=True)
+plt.show()
